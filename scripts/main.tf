b2e7ac1a8cc26be2b7e178eb7be68713b7d22456
Jesse Hall <jessehall3@gmail.com>
2022-01-13 08:10:47 -0600

    mongo database added to cluster

--- scripts/main.tf
+++ scripts/main.tf
@@ -9,6 +9,11 @@ terraform {
       source = "hashicorp/tls"
       version = "3.1.0"
     }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.7.1"
+    }
   }
 
   required_version = ">= 0.14"
